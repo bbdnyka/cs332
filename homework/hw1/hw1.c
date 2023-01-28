bfeebae3fc6d9ea7e0928d9199c1fe2215fcc266
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-01-28 16:51:26 +0000

    hw1.c

--- homework/hw1/hw1.c
+++ homework/hw1/hw1.c
@@ -1,8 +1,17 @@
 #include "hw1.h"
 
 int main()
-{
-   // intro332532(15);
-    //UABNumber(28);
-    reverseNum(1234);
+{   
+    //intro332532(15);
+
+    //printf("%d \n",UABNumber(27));
+
+    //printf("%d \n",reverseNum(1234));
+
+    int inp[] = {1,3,2,5,4};
+    int size = 5;
+    //printf("%d \n",smallerThanIndex(inp,5));
+
+    //printf("%d \n",*arrayDetails(inp,size));
+    arrayDetails(inp,size);
 }
