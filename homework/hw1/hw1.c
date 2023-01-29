83ad7c4e7cdc9a4ba6029f4ed596cc17177ef694
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-01-29 21:00:43 +0000

    hw1.c

--- homework/hw1/hw1.c
+++ homework/hw1/hw1.c
@@ -1,17 +1,22 @@
 #include "hw1.h"
 
 int main()
-{   
+{  
+    /*intro332532*/ 
     intro332532(30);
-    
+
+    /*UABNumber*/ 
     printf("UABNumber= %d \n",UABNumber(0));
 
+    /*reverseNum*/
     printf("reverseNum= %d \n",reverseNum(29));
 
+    /*smallerThanIndex*/
     int inp1[]= {1,2,0,44,29,309};
     int inp1Size = 6;
     printf("smallerThanIndex= %d \n",smallerThanIndex(inp1,inp1Size));
 
+    /*arrayDetails*/
     int inp[] = {-8,-23,18,103,0,1,-4,631,3,-41,5};
     int size = 11;
     int i;
