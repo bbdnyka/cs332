a128d70f3d452cc3fdb45c6e8d1f0f2007e1d451
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-01-29 21:10:59 +0000

    hw1.h

--- homework/hw1/hw1.h
+++ homework/hw1/hw1.h
@@ -31,118 +31,118 @@ void intro332532(int n)
         printf("\n");
     }
 }
-    /*Checks if input is a UAB NUmber*/
-    int UABNumber(int n2)
-    {
-        printf("Enter a number here: ");
-        scanf("%d", &n2);
-        int divisors=0 ;
-        int i;
+/*Checks if input is a UAB NUmber*/
+int UABNumber(int n2)
+{
+    printf("Enter a number here: ");
+    scanf("%d", &n2);
+    int divisors=0 ;
+    int i;
 
-        /*Iterates through input number and determines which numbers are divisors or the input*/
-        for(i=1;i<n2;i++)
+    /*Iterates through input number and determines which numbers are divisors or the input*/
+    for(i=1;i<n2;i++)
+    {
+        if(n2 % i==0)//input divisors added to variable divisors
+        {  
+        divisors += i;
+        } 
+    }
+    /*Checks if number stored in divisor is equal to input number */
+    for(i=divisors;i>0;i--)
+    {
+        if(divisors==n2)//If equal returns true(1)
         {
-          if(n2 % i==0)//input divisors added to variable divisors
-          {  
-            divisors += i;
-          } 
-        }
-        /*Checks if number stored in divisor is equal to input number */
-        for(i=divisors;i>0;i--)
-        {
-          if(divisors==n2)//If equal returns true(1)
-          {
-            return 1;
-          } 
-          else{//if not equal returns false(0)
-            return 0;
-          }
+        return 1;
+        } 
+        else{//if not equal returns false(0)
+        return 0;
         }
     }
-    /*Take a number and returns the reverse. */
-    int reverseNum(int n3)
-    {
+}
+/*Take a number and returns the reverse. */
+int reverseNum(int n3)
+{
     int i;
     int temp;
     int reverse;
     int a = n3;
-        /*Iterates through given number until it equals 0.*/
-         while(a!=0)
-        {
-            temp = a%10;//gets individual nums through iterations
-            reverse=reverse * 10 +temp;//iterate through different place values and adds temp
-            a = a/10;//decrements a until 0 for counter;
-        }
+    /*Iterates through given number until it equals 0.*/
+        while(a!=0)
+    {
+        temp = a%10;//gets individual nums through iterations
+        reverse=reverse * 10 +temp;//iterate through different place values and adds temp
+        a = a/10;//decrements a until 0 for counter;
+    }
     return reverse;
-    }
+}
 
-    int smallerThanIndex(int* numbers, int size)
+int smallerThanIndex(int* numbers, int size)
+{
+    int i;
+    int* num = numbers;
+    int smallInd=0;
+    for(i=0;i<size;i++)
     {
-        int i;
-        int* num = numbers;
-        int smallInd=0;
-        for(i=0;i<size;i++)
+        if(*(num+i)<i)
         {
-            if(*(num+i)<i)
-            {
-                smallInd += 1;
-                
-            }
-        }
-        return smallInd;
-    }
-    int* arrayDetails(int *arr, int size)
-    {
-        int i,j,k;
-        int *newArr;
-        int newArrSize = 6;
-        
-        newArr=malloc(newArrSize*8);
-
-        int arrSize=size;
-        int arrMin;
-        int arrMinInd;
-        double arrMean;
-        int arrMax;
-        int arrMaxInd;
-
-        int temp=arr[0];
-        int temp2=arr[0];
-            for(i=0;i<size;i++)
-            {   
-                if(arr[i]<temp)
-                {
-                    temp=arr[i];
-                    arrMin=temp;
-                    arrMinInd=i;  
-
-                }
-            for(j=0;j<size;j++)
-                {
-                    arr[j];
-                    if(arr[j]>temp2)
-                    {
-                        temp2=arr[j];
-                        arrMax=temp2;
-                        arrMaxInd=j;
-                    }
-                }
-            for(k=0;k<size;k++)
-                {
-                    arrMean+=arr[k];
-                }
-                arrMean/=size;
-            }
+            smallInd += 1;
             
-            *(newArr+0)=arrSize;
-            *(newArr+1)=arrMin;
-            *(newArr+2)=arrMinInd;
-            *(newArr+3)=arrMean;
-            *(newArr+4)=arrMax;
-            *(newArr+5)=arrMaxInd;
-
-            return newArr;
-            free(newArr);
         }
-
+    }
+    return smallInd;
+}
+int* arrayDetails(int *arr, int size)
+{
+    int i,j,k;
+    int *newArr;
+    int newArrSize = 6;
     
+    newArr=malloc(newArrSize*8);
+
+    int arrSize=size;
+    int arrMin;
+    int arrMinInd;
+    double arrMean;
+    int arrMax;
+    int arrMaxInd;
+
+    int temp=arr[0];
+    int temp2=arr[0];
+        for(i=0;i<size;i++)
+        {   
+            if(arr[i]<temp)
+            {
+                temp=arr[i];
+                arrMin=temp;
+                arrMinInd=i;  
+
+            }
+            for(j=0;j<size;j++)
+            {
+                arr[j];
+                if(arr[j]>temp2)
+                {
+                    temp2=arr[j];
+                    arrMax=temp2;
+                    arrMaxInd=j;
+                }
+            }
+            for(k=0;k<size;k++)
+            {
+                arrMean+=arr[k];
+            }
+            arrMean/=size;
+        }
+        
+        *(newArr+0)=arrSize;
+        *(newArr+1)=arrMin;
+        *(newArr+2)=arrMinInd;
+        *(newArr+3)=arrMean;
+        *(newArr+4)=arrMax;
+        *(newArr+5)=arrMaxInd;
+
+        return newArr;
+        free(newArr);
+    }
+
+
