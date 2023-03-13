85c3c4849a6315fce05d780b5b900041127a8846
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-03-13 20:48:10 +0000

    homework

--- homework/hw2/hw2.c
+++ homework/hw2/hw2.c
@@ -20,6 +20,7 @@ Date: ___19february23_________
 #include <time.h>
 // malloc input for traversal
 #define MAX_PATH_SIZE 2000
+
 //cases for file types
 char *fileType(unsigned char type){
     char *str;
@@ -36,136 +37,138 @@ char *fileType(unsigned char type){
   }
   return str;
 }
-// function for directory traversal    
-void traverseDir (char* path, int tabSpaces){
-    struct dirent *dirent;// used for d_type
+/*
+directory traversal
+path name changes with each recurse of directory
+*/  
+void traverseDir(char* path, int tabSpaces,int flags){
+    struct dirent *dtype;// used for d_type
     DIR *parentDir;// used for opendir
-
-    // First, we need to open the directory.
-    parentDir = opendir(path);
-    // if file name typed wrong, error prints
+    parentDir = opendir(path); /*Open directory */
+    /*if file name typed wrong, error prints*/
     if (parentDir == NULL) { 
         printf ("Error opening directory '%s'\n", path); 
         exit (-1);
     }
     int count = 1;
-    // After we open the directory, we can read the contents of the directory, file by file.
-    while((dirent = readdir(parentDir)) != NULL){ 
-
+    struct stat *sb = malloc(sizeof(struct stat));
+    /*
+    read the contents of the directory, file by file.
+    */
+    while((dtype =readdir(parentDir)) != NULL){ 
         // If the file's name is "." or "..", ignore them. We do not want to infinitely recurse.
-        if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0) {
+        if (strcmp(dtype->d_name, ".") == 0 || strcmp(dtype->d_name, "..") == 0) {
             continue;
+        } 
+        /*
+        copy of path
+        concatenate file name to copyPath
+        (HW2/file name )
+        */
+        char* copyPath = malloc(MAX_PATH_SIZE);
+        strcpy(copyPath, path);
+        strcat(copyPath, "/");
+        strcat(copyPath,dtype->d_name);
+        lstat(copyPath,sb); /*copyPath added to sb*/ 
+        /*Empties copyPath after added to sb*/
+        // strcpy(copyPath, " ");
+        // memset(copyPath,0,strlen(copyPath));
+        // file format
+        if(flags==1){// S
+        printf("%*s[%d] %s (%s) (Octal Permission: %o, File size: %ld, Access time: %s)\n", 4 * tabSpaces, " ",
+        count, dtype->d_name, fileType(dtype->d_type),sb->st_mode, sb->st_size,ctime(&sb->st_atime)); 
         }
-
-    // Print the formated file.
-        printf("%*s[%d] %s (%s)\n", 4 * tabSpaces, " ", count, dirent->d_name, fileType(dirent->d_type)); 
-    // count for numbering
-        count++; 
-    }
+        if(flags==2){// s
+            printf("s flag = %d\n",flags);
+            break;
+        }
+        if(flags==3){// f
+            printf("f flag = %d\n",flags);
+            break;
+        }
+        break;
+        if(flags== 0){// no dir spec
+            printf("no directory specified. flag = %d\n",flags);
+            printf();
+            break;
+        }
+        count++;// count for numbering   
     
+// Check to see if the file type is a directory. If it is, recursively call traverseDirectory on it.
+        if (dtype->d_type == DT_DIR) {
+            // Build the new file path.
+            char *subDirPath = (char *) malloc(MAX_PATH_SIZE);
+            strcpy(subDirPath, path);
+            strcat(subDirPath, "/");
+            strcat(subDirPath, dtype->d_name);
+            traverseDir(subDirPath, tabSpaces + 1,flags);
+            free(subDirPath); 
+        }
+        free(copyPath);
+    }
+    free(sb);       
 }
 int main(int argc, char **argv) {
-    char* filename;// stores argv[1]
     struct stat sb;// used for stat print
-    int flagS,flags, flagf, opt;// flags and getop variable
-    flagS = 0;// [-S]
-    flags = 0;// [-s]
-    flagf = 0;// [-f]
+    /* opt = getopt, nsecs = file size, flag1 = S, flag2 = s, flag 3 = f */
+    int opt,flag1=0,flag2=0,flag3=0;// flags and getop variable
     int tabSpaces = 0;// tabs for traveerseDIR
+    char* fin;
+    int flags = 0;
+    int size;
 
-// check flags
-    while((opt = getopt(argc, argv, "f;Ss;")) != -1){
+
+    // check flags
+    while((opt = getopt(argc, argv, "f:Ss:")) != -1){
         //printf("%c\n",opt);
         switch(opt){
            
         // List all files in the hierarchy that 1) file name contains substring in string pattern.
         // 2) the depth of the filerelative to starting traversal is <= depth option. (starting directory is 0)
             case 'S':
-        // flagS used to access function ttraverseDIR, stats cases
-            flagS = 1;
+            flag1=1;
             break;
-        // List files in hierarchy 
+        // This should list all files in the hierarchy with file size less than or equal to the value 
+        // specified
             case 's':
-        // flags used to access function ttraverseDIR
-            flags = 1;
+            size = atoi(optarg);
+            flag2=1;
             break;
-        // List files in hierarchy
+        // 1) the file name contains the substring in the string pattern option, AND 2) the depth of the file 
+        // relative to the starting directory of the traversal is less than or equal to the depth option. 
             case 'f':
-        // flagf used to access function ttraverseDIR
-            flags = 1;
+            fin = strtok(optarg, " ");
+            flag3=1;
             break;
-        // if anything other than implemented flags used, error prints
-            default: /* '?' */
-                fprintf(stderr, "Usage: %s name [-S] [-s] [-f] \n",*(argv+0));
-                exit(EXIT_FAILURE);
-            
         }
-    // [-S] flag
-        if(flagS == 1){
-            // nothing entered
-            if (stat(*(argv+1), &sb) == -1) {
-                    perror("stat");
-                    exit(EXIT_FAILURE);
-                }
-        // stores file name
-            filename = argv[1];
-        // stores stat info
-            long fileMode = 0;
-            long fileLink = 0;
-            long long fileSize = 0;
-            long long fileAccess = 0;
-            printf("File type: ");
-        // cases for stats
-            switch (sb.st_mode & S_IFMT) {
-                case S_IFBLK:  printf("block device\n");            break;
-                case S_IFCHR:  printf("character device\n");        break;
-                case S_IFDIR:  printf("directory\n");               break;
-                case S_IFIFO:  printf("FIFO/pipe\n");               break;
-                case S_IFLNK:  printf("symlink\n");                 break;
-                case S_IFREG:  printf("regular file\n");            break;
-                case S_IFSOCK: printf("socket\n");                  break;
-                default:       printf("unknown?\n");                break;
-                }
-        //stats stored
-            fileMode = sb.st_mode;
-            fileLink = sb.st_nlink;
-            fileSize = sb.st_size;
-            fileAccess = sb.st_atime;
-        //stats printed
-            printf("File name: %s (Permission bits: %lo. Files in hierarchy: %ld. File size: %lld bytes. Time accessed: %s.)",filename,fileMode,fileLink,fileSize,ctime(&fileAccess));
-            printf("\n");
-        //function call for subpaths
-            traverseDir(*(argv+1), tabSpaces+1);
-            //}
-        //exit code to stop compile
-            exit(EXIT_SUCCESS);
-        }
-    // [-s] flag
-        if(flags == 1){
-    //function call for subpaths 
-            traverseDir(*(argv+1), tabSpaces+1);
-        } 
-    // [-f] flag
-        if(flagf == 1){
-    //function call for subpaths
-            traverseDir(*(argv+1), tabSpaces+1);
-        }   
     }
-    // Check to see if the user provides at least 2 command-line-arguments.
-    if (argc < 2) { 
-        printf ("Usage: %s <dirname>\n", *(argv+0)); 
-        exit(-1);
+    if(flag1==1){// S
+        traverseDir(argv[optind],tabSpaces, flags+1);
     }
-    else if(argc > 2){
-        printf("Too many arguments.\n");
+    if(flag2==1){// s
+        printf("%d Bytes\n",size);
+        traverseDir(argv[optind],tabSpaces,flags+2);
+    }
+    if(flag3==1){// f
+        printf("%s = ", fin);
+        traverseDir(argv[optind],tabSpaces,flags+3);
     }
     else{
-        printf("The argument supplied is %s\n", *(argv+1));
+        flags=0;
     }
-// function call for directory and sub paths
-    traverseDir(argv[1], tabSpaces+1);
-} 
+    if (optind == argc) {
+        // No directory was specified.
+        traverseDir(".", tabSpaces, flags);
+    }
+    else {
+        // A directory was specified.
+        traverseDir(argv[argc - 1], tabSpaces, flags);
+    }
+}
 
+
+
+    
 
 
 
